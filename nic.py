15b7dd2304a9650470caeaa082f72ab3f25c3344
jisdulanjana <93591453+jisdulanjana@users.noreply.github.com>
2023-12-01 17:11:12 +0530

    gender

--- nic.py
+++ nic.py
@@ -12,12 +12,19 @@ def validate(nic : str) -> bool:
 def identify_born_year(nic : str) -> int:
     year = nic[0:4]
     return int(year)
-
-
+# check gender
+def checker_gender(nic : str) -> str:
+    gender = int(nic[4:7])
+    if gender > 500:
+        return "Female"
+    else:
+        return "Male"
 
 # Get born Date
 def get_born_date(nic : str) -> datetime.date :
     born_day = int(nic[4:7])
+    if born_day > 500:
+        born_day -= 500
     year = identify_born_year(nic)
     jan_first = datetime.date(year, 1, 1)
     born_date = jan_first + datetime.timedelta(days=born_day -1)
@@ -35,9 +42,13 @@ if not valid_nic:
 # identift the born year
 year = identify_born_year(nic_number)
 
+# gender
+gender = checker_gender(nic_number)
+
 # Calculate the exact born date
 born_date = get_born_date(nic = nic_number)
 day = born_date.strftime("%A")
 # print the output
-print(f"Your birthday : {born_date}")
-print(f'your born day is {day}')
+print(f" birthday : {born_date}")
+print(f'born day is {day}')
+print(f'Gender is {gender}')
